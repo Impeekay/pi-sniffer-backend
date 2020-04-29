0d91c1a9d5f8e287fc91e36a82b5e46ac7b56462
Pavan Kumar <kumarpavan1309@gmail.com>
2020-04-29 13:25:00 +0530

    migration script changes

--- scripts/convertHourlyToDaily.js
+++ scripts/convertHourlyToDaily.js
@@ -1,33 +1,28 @@
 const moment = require("moment");
 const fs = require("fs");
 let filePath = "../data/";
+let tmpPath = "../newData/";
 
 try {
   const files = fs.readdirSync(filePath);
-  console.log(files);
+  // console.log(files);
   if (!!!files) {
     console.log("Empty directory exiting");
     process.exit(1);
+  } else {
+    fs.mkdirSync(tmpPath);
   }
-  files.forEach(file => {
+  files.forEach((file) => {
     // console.log(moment(file).toDate());
-    let newFileName = moment(file)
-      .startOf("day")
-      .toISOString();
-    fs.open(`${filePath}${newFileName}`, "a", (err, fd) => {
-      if (err) throw err;
-      fs.readFile(`${filePath}${file}`, (err, data) => {
-        if (err) throw err;
-        fs.appendFile(fd, data.toString(), "utf8", err => {
-          if (err) throw err;
-          fs.unlink(`${filePath}${file}`, err => {
-            if (err) throw err;
-            console.log("Deleted old file");
-          });
-        });
-      });
-    });
+    let newFileName = moment(file).startOf("day").toISOString();
+    const fd = fs.openSync(`${tmpPath}${newFileName}`, "a");
+    let data = fs.readFileSync(`${filePath}${file}`);
+    console.log(`appending ${file} to ${newFileName}`);
+    fs.appendFileSync(fd, data.toString(), "utf8");
+    fs.closeSync(fd);
   });
+  fs.rmdirSync(filePath, { recursive: true }); //remove the old directory
+  fs.renameSync(tmpPath, filePath);
 } catch (error) {
   console.log(error);
 }
